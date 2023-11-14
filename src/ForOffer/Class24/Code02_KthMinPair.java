1c27d4fcb84b47a5d20373f6d6fcd9a3c8766f24
Neure6676 <1120708221@qq.com>
2023-11-14 14:53:09 -0500

    Nov14

--- src/ForOffer/Class24/Code02_KthMinPair.java
+++ src/ForOffer/Class24/Code02_KthMinPair.java
@@ -16,6 +16,63 @@ public class Code02_KthMinPair {
         // 在无序数组中，找到第K小的数，返回值
         // 第K小，以1作为开始
         int firstNum = getMinKth(arr, (k - 1) / N);
-        //
+        // 第一位数字找到
+        int lessFirstNumSize = 0; // 小于第一位的数有几个
+        int firstNumSize = 0; // 等于第一位的数有几个
+        for (int i = 0; i < N; i++) {
+            if (arr[i] < firstNum) {
+                lessFirstNumSize++;
+            }
+            if (arr[i] == firstNum) {
+                firstNumSize++;
+            }
+        }
+        int rest = k - (lessFirstNumSize * N);
+        return new int[] {firstNum, getMinKth(arr, (rest - 1) / firstNumSize)};
+    }
+
+    // 改写快排，时间复杂度O(N)
+    // 在无序数组arr中，找到，如果排序的话，arr[index]的数是什么？
+    public static int getMinKth(int[] arr, int index) {
+        int L = 0;
+        int R = arr.length - 1;
+        int pivot = 0;
+        int[] range = null;
+        while (L < R) {
+            // choose a random pivot
+            pivot = arr[L + (int)(Math.random() * (R - L + 1))];
+            range = partition(arr, L, R, pivot);
+            if (index < range[0]) {
+                R = range[0] - 1;
+            } else if (index > range[1]) {
+                L = range[1] + 1;
+            } else {
+                return pivot;
+            }
+        }
+        return arr[L];
+    }
+
+    // 荷兰国旗问题分区
+    public static int[] partition(int[] arr, int L, int R, int pivot) {
+        int less = L - 1;
+        int more = R + 1;
+        int cur = L;
+        while (cur < more) {
+            if (arr[cur] < pivot) {
+                swap(arr, ++less, cur++);
+            } else if (arr[cur] > pivot) {
+                swap(arr, cur, --more);
+            } else {
+                cur++;
+            }
+        }
+        return new int[] {less + 1, more - 1};
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
---
+++ src/SystematicClass/Class30/Code01_GetMinKth.java
@@ -0,0 +1,49 @@
+package SystematicClass.Class30;
+
+public class Code01_GetMinKth {
+
+    // 改写快排，时间复杂度O(N)
+    // 在无序数组arr中，找到，如果排序的话，arr[index]的数是什么？
+    public static int getMinKth(int[] arr, int index) {
+        int L = 0;
+        int R = arr.length - 1;
+        int pivot = 0;
+        int[] range = null;
+        while (L < R) {
+            // choose a random pivot
+            pivot = arr[L + (int)(Math.random() * (R - L + 1))];
+            range = partition(arr, L, R, pivot);
+            if (index < range[0]) {
+                R = range[0] - 1;
+            } else if (index > range[1]) {
+                L = range[1] + 1;
+            } else {
+                return pivot;
+            }
+        }
+        return arr[L];
+    }
+
+    // 荷兰国旗问题分区
+    public static int[] partition(int[] arr, int L, int R, int pivot) {
+        int less = L - 1;
+        int more = R + 1;
+        int cur = L;
+        while (cur < more) {
+            if (arr[cur] < pivot) {
+                swap(arr, ++less, cur++);
+            } else if (arr[cur] > pivot) {
+                swap(arr, cur, --more);
+            } else {
+                cur++;
+            }
+        }
+        return new int[] {less + 1, more - 1};
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
