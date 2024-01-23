dfd04c0c4d7cccce26a022b34988403f0f95d7e4
Neure6676 <1120708221@qq.com>
2024-01-22 21:46:00 -0500

    Jan22 trie

--- src/UltimateAlgo2024/Tree/Trie/Code01_TrieTree.java
+++ src/UltimateAlgo2024/Tree/Trie/Code01_TrieTree.java
@@ -1,2 +1,166 @@
-package UltimateAlgo2024.Tree.Trie;public class Code01_TrieTree {
+package UltimateAlgo2024.Tree.Trie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// https://leetcode.com/problems/implement-trie-ii-prefix-tree/
+public class Code01_TrieTree {
+
+    class Trie {
+
+        public static int MAXN = 2001;
+
+        public static int[][] trie = new int[MAXN][26];
+        public static int[] end = new int[MAXN];
+        public static int[] pass = new int[MAXN];
+        public static int cnt;
+
+
+        public Trie() {
+            clear();
+            cnt = 1;
+        }
+
+        public void insert(String word) {
+            int cur = 1;
+            pass[cur]++;
+            for (int i = 0, path; i < word.length(); i++) {
+                path = word.charAt(i) - 'a';
+                if (trie[cur][path] == 0) {
+                    trie[cur][path] = ++cnt;
+                }
+                cur = trie[cur][path];
+                pass[cur]++;
+            }
+            end[cur]++;
+        }
+
+        public int countWordsEqualTo(String word) {
+            int cur = 1;
+            for (int i = 0, path; i < word.length(); i++) {
+                path = word.charAt(i) - 'a';
+                if (trie[cur][path] == 0) {
+                    return 0;
+                }
+                cur = trie[cur][path];
+            }
+            return end[cur];
+        }
+
+        public int countWordsStartingWith(String prefix) {
+            int cur = 1;
+            for (int i = 0, path; i < prefix.length(); i++) {
+                path = prefix.charAt(i) - 'a';
+                if (trie[cur][path] == 0) {
+                    return 0;
+                }
+                cur = trie[cur][path];
+            }
+            return pass[cur];
+        }
+
+        public void erase(String word) {
+            if (countWordsEqualTo(word) > 0) {
+                int cur = 1;
+                pass[cur]--;
+                for (int i = 0, path; i < word.length(); i++) {
+                    path = word.charAt(i) - 'a';
+                    if (--pass[trie[cur][path]] == 0) {
+                        trie[cur][path] = 0;
+                        return;
+                    }
+                    cur = trie[cur][path];
+                }
+                end[cur]--;
+            }
+        }
+
+        public static void clear() {
+            for (int i = 1; i <= cnt; i++) {
+                Arrays.fill(trie[i], 0);
+                end[i] = 0;
+                pass[i] = 0;
+            }
+        }
+    }
+
+    class Trie1 {
+
+        class TrieNode {
+            public int pass;
+            public int end;
+            public TrieNode[] nexts;
+
+            public TrieNode() {
+                pass = 0;
+                end = 0;
+                nexts = new TrieNode[26];
+            }
+        }
+
+        private TrieNode root;
+
+        public Trie1() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            node.pass++;
+            for (int i = 0, path; i < word.length(); i++) { // 从左往右遍历字符
+                path = word.charAt(i) - 'a'; // 由字符，对应成走向哪条路
+                if (node.nexts[path] == null) {
+                    node.nexts[path] = new TrieNode();
+                }
+                node = node.nexts[path];
+                node.pass++;
+            }
+            node.end++;
+        }
+
+        // 如果之前word插入过前缀树，那么此时删掉一次
+        // 如果之前word没有插入过前缀树，那么什么也不做
+        public void erase(String word) {
+            if (countWordsEqualTo(word) > 0) {
+                TrieNode node = root;
+                node.pass--;
+                for (int i = 0, path; i < word.length(); i++) {
+                    path = word.charAt(i) - 'a';
+                    if (--node.nexts[path].pass == 0) {
+                        node.nexts[path] = null;
+                        return;
+                    }
+                    node = node.nexts[path];
+                }
+                node.end--;
+            }
+        }
+
+        // 查询前缀树里，word单词出现了几次
+        public int countWordsEqualTo(String word) {
+            TrieNode node = root;
+            for (int i = 0, path; i < word.length(); i++) {
+                path = word.charAt(i) - 'a';
+                if (node.nexts[path] == null) {
+                    return 0;
+                }
+                node = node.nexts[path];
+            }
+            return node.end;
+        }
+
+        // 查询前缀树里，有多少单词以pre做前缀
+        public int countWordsStartingWith(String pre) {
+            TrieNode node = root;
+            for (int i = 0, path; i < pre.length(); i++) {
+                path = pre.charAt(i) - 'a';
+                if (node.nexts[path] == null) {
+                    return 0;
+                }
+                node = node.nexts[path];
+            }
+            return node.pass;
+        }
+
+    }
 }
--- src/UltimateAlgo2024/Tree/Trie/Code02_TwoNumbersMaximumXor.java
+++ src/UltimateAlgo2024/Tree/Trie/Code02_TwoNumbersMaximumXor.java
@@ -1,2 +1,102 @@
-package UltimateAlgo2024.Tree.Trie;public class Code02_TwoNumbersMaximumXor {
+package UltimateAlgo2024.Tree.Trie;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+// https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
+public class Code02_TwoNumbersMaximumXor {
+
+    public int findMaximumXOR(int[] nums) {
+        build(nums);
+        int ans = 0;
+        for (int num : nums) {
+            ans = Math.max(ans, maxXor(num));
+        }
+        clear();
+        return ans;
+    }
+
+    public static int MAXN = 3000001;
+
+    public static int[][] tree = new int[MAXN][2];
+
+    public static int cnt;
+
+    // 数字只需要从哪一位开始考虑
+    public static int high;
+
+    public static void build(int[] nums) {
+        cnt = 1;  // didnt use 0
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(num, max);
+        }
+        // 计算数组最大值的二进制状态，有多少个前缀的0
+        // 可以忽略这些前置的0，从left位开始考虑
+        high = 31 - Integer.numberOfLeadingZeros(max);
+        for (int num : nums) {
+            insert(num);
+        }
+    }
+
+    public static void insert(int num) {
+        int cur = 1;
+        for (int i = high, path; i >= 0; i--) {
+            path = (num >> i) & 1;
+            if (tree[cur][path] == 0) {
+                tree[cur][path] = ++cur;
+            }
+            cur = tree[cur][path];
+        }
+    }
+
+    public static int maxXor(int num) {
+        int ans = 0;
+        int cur = 1;
+        for (int i = high, status, best; i >= 0; i--) {
+            status = (num >> i) & 1;
+            best = status ^ 1;
+            if (tree[cur][best] == 0) {
+                // can't reach the best case
+                best ^= 1;
+            }
+            ans |= (status ^ best) << i;
+            cur = tree[cur][best];
+        }
+        return ans;
+    }
+
+    public static void clear() {
+        for (int i = 0; i < cnt; i++) {
+            Arrays.fill(tree[i], 1);
+        }
+    }
+
+
+
+    // HashMap
+    public int findMaximumXOR2(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int ans = 0;
+        HashSet<Integer> set = new HashSet<>();
+        int left = 31 - Integer.numberOfLeadingZeros(max);
+        for (int i = left; i >= 0; i--) {
+            // ans: 31-i+1 done
+            int better = ans | (1 << i);
+            set.clear();
+            for (int num : nums) {
+                // only care 31 ... i,  put rest 0
+                num = (num >> i) << i;
+                set.add(num);
+                if (set.contains(num ^ better)) {
+                    ans = better;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
 }
