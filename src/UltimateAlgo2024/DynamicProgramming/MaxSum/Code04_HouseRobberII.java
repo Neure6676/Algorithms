4a8c2e3c8276214336fa9bd069d7099d58ceb1ee
Neure6676 <1120708221@qq.com>
2024-02-22 23:34:38 -0500

    Feb19 dp max sum part1

---
+++ src/UltimateAlgo2024/DynamicProgramming/MaxSum/Code04_HouseRobberII.java
@@ -0,0 +1,33 @@
+package UltimateAlgo2024.DynamicProgramming.MaxSum;
+
+// https://leetcode.com/problems/house-robber-ii/
+public class Code04_HouseRobberII {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        return Math.max(best(nums, 1, n - 1), nums[0] + best(nums, 2, n - 2));
+    }
+
+    public static int best(int[] arr, int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+        if (l == r) {
+            return arr[l];
+        }
+        if (r - l == 1) {
+            return Math.max(arr[l], arr[r]);
+        }
+        int prePre = arr[l];
+        int pre = Math.max(arr[l], arr[l + 1]);
+        for (int i = l + 2; i <= r; i++) {
+            int cur = Math.max(pre, prePre + arr[i]);
+            prePre = pre;
+            pre = cur;
+        }
+        return pre;
+    }
+}
